3629552f91cae07efb666f60338d4b26b17aba15
Robin Erlacher <rerlache@gmail.com>
2023-04-10 01:02:48 +0200

    refinement of navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,27 +1,28 @@
-import {
-    BrowserRouter as Router,
-    Route,
-    Routes,
-    NavLink,
-  } from "react-router-dom";
+import { BrowserRouter, Route, Routes, NavLink } from "react-router-dom";
 import BlackJack from "../features/blackJack/components/blackJack";
-import CardWar from '../features/cardWar/components/CardWar'
+import CardWar from "../features/cardWar/components/CardWar";
 
 function Navigation() {
   return (
     <>
-      <Router>
+      <BrowserRouter>
         <div className="nav-container">
-          <NavLink to="/" className='nav-item'>Home</NavLink>
-          <NavLink to="/cardwar" className='nav-item'>Card-War</NavLink>
-          <NavLink to="/blackjack" className='nav-item'>BlackJack</NavLink>
+          <NavLink to="/" className="nav-item">
+            Home
+          </NavLink>
+          <NavLink to="/cardwar" className="nav-item">
+            Card-War
+          </NavLink>
+          <NavLink to="/blackjack" className="nav-item">
+            BlackJack
+          </NavLink>
         </div>
         <Routes>
-            <Route exact path="/" />
-            <Route exact path="/cardwar" element={<CardWar />} />
-            <Route exact path="/blackjack" element={<BlackJack />} />
+          <Route exact path="/" />
+          <Route exact path="/cardwar" element={<CardWar />} />
+          <Route exact path="/blackjack" element={<BlackJack />} />
         </Routes>
-      </Router>
+      </BrowserRouter>
     </>
   );
 }
